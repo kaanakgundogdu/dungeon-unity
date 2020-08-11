ca2f1b59571145b32f9234d48c7a23e3ef900f0c
Kaan Akgundogdu <kaanakgundogdu@gmail.com>
2020-08-11 18:44:29 +0300

    Dungeon

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private float _moveSpeed = 2;
+	private Rigidbody2D _myRig;
+
+	private void Start()
+	{
+		_myRig = gameObject.GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		float myInput = Input.GetAxisRaw("Horizontal");
+
+
+		if (myInput <=0 )
+		{
+			_myRig.velocity = new Vector2(  myInput * _moveSpeed, _myRig.velocity.y);
+		}
+
+		if (myInput > 0)
+		{
+			_myRig.velocity = new Vector2(myInput * _moveSpeed, _myRig.velocity.y);
+		}
+
+	}
+
+
+}
